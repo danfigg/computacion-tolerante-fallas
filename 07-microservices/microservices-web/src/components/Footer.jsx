c1c91dd9e59db8ceffc47daebf5591c37418cf22
danfigg <dangarciafigueroa19@gmail.com>
2025-05-01 17:45:07 -0600

    style: change name footer

--- 07-microservices/microservices-web/src/components/Footer.jsx
+++ 07-microservices/microservices-web/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} MiApp. Todos los derechos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} Barbería Élite. Todos los derechos reservados.</p>
     </footer>
   );
 }
